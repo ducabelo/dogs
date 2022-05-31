3e50da47afe37794aa27f904fbf96f62565d506e
Eduardo Assis <eduardohenriqueassis1973@gmail.com>
2022-05-30 23:19:20 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Footer from './Components/Footer';
+import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
+import User from './Components/User/User';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
+
 function App() {
-  return <div>App React</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={<ProtectedRoute>{<User />}</ProtectedRoute>}
+            />
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
